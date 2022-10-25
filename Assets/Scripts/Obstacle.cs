a042d68ce8b6227d5791ec327557ee7613dad9fc
03-Shafiq <fiq-chelseapro@hotmail.com>
2022-10-25 12:53:11 +0800

    script update

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,17 +1,34 @@
 ﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //This script manages the behavior of individual obstacle
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] private float Speed = 3;
+  
 
     void Update()
     {
         if (transform.position.x <= -8)
+        {
             Destroy(gameObject);
+          
+        }
+        
         else
             transform.Translate(Vector3.right * Time.deltaTime * -Speed);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+          
+            Destroy(collision.gameObject);
+            SceneManager.LoadScene("Gameover");
+
+        }
     }
 }
